3986ca7a90318c60cb4e8b1846f70a5415b33d43
Cristian <cristian@swapps.com>
2020-05-03 16:48:48 -0500

    refactor: Update client for new status structure

--- games/static/components/app.js
+++ games/static/components/app.js
@@ -16,32 +16,37 @@ class Game extends React.Component {
     const component = this;
     this.props.gameSocket.onmessage = function (e) {
       const data = JSON.parse(e.data);
-      const currentPlayer = data["next_player"] ? "player_1" : "player_2";
+      const currentPlayer = data["next_player"]
+        ? data["player_1"]
+        : data["player_2"];
 
-      if (!data.started) {
-        component.setState({
-          message: "Awaiting for another player to start the game...",
-        });
-      } else if (data["finished"]) {
-        const winner_message = data["winner"]
-          ? `The winner is player 1: ${data["player_1"]}`
-          : data["winner"] === false
-          ? `The winner is player 2: ${data["player_2"]}`
-          : "This game was a draw.";
-        component.setState({
-          message: `This game has finished. ${winner_message}`,
-        });
-      } else {
-        // Game in progress
-        if (data[currentPlayer] === component.props.username) {
+      switch (data.status) {
+        case "PENDING":
           component.setState({
-            message: "It is your turn to make a move!",
+            message: "Awaiting for another player to start the game...",
           });
-        } else {
+          break;
+        case "FINISHED":
+          const winner_message = data["winner"]
+            ? `The winner is player 1: ${data["player_1"]}`
+            : data["winner"] === false
+            ? `The winner is player 2: ${data["player_2"]}`
+            : "This game was a draw.";
           component.setState({
-            message: `Now it is ${currentPlayer}'s turn`,
+            message: `This game has finished. ${winner_message}`,
           });
-        }
+          break;
+        default:
+          // Game in progress
+          if (currentPlayer === component.props.username) {
+            component.setState({
+              message: "It is your turn to make a move!",
+            });
+          } else {
+            component.setState({
+              message: `Now it is ${currentPlayer}'s turn`,
+            });
+          }
       }
       component.setState({ board: data.board, moves: data.moves });
     };
